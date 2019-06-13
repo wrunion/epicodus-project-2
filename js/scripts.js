4aaf3321d11361fff2bf4d0818d9adfa3838858d
Winter Runion <winterrunion@gmail.com>
2019-06-13 11:11:55 -0700

    Debug JS

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
       $("#free").show();
       $("#the-tech-academy").show();
       $("#end-message").show();
+      $("#end-nav").show();
     } else if (location === "Portland") {
       $("form#quiz2").show();
     }
@@ -21,36 +22,68 @@ $(document).ready(function() {
   $("form#quiz2").submit(function(event) {
 
     $("form#quiz2").hide();
-    // $("#results-msg").show();
+    $("#results-msg").show();
+    $("#end-nav").show();
 
-    var experience = $("#experience").val();
+    var goal = $("#goal").val();
     var cost = $("#amount").val();
-    var isFree = $("#goal").val();
-    var language = ("#language").val();
+    var experience = $("#experience").val();
+    var language = $("#language").val();
+    var learningEnvironment = $("#online-or-irl").val();
 
-      //   if (isFree === "free") {
-      //   $("#free").show();
-      //   $("#end-nav").show();
-      // }
+    if (goal === "hobby" || cost != "code-camp") {
+      $("#free").show();
+    }
 
-    if (experience === "advanced") {
-      $("#alchemy-code-lab").show();
+    if (learningEnvironment === "online") {
+      $("#free").show();
+      $("div.online-result").show();
+      $("#end-message").show();
+    }
+     else if (experience === "advanced") {
+      $("#free").show();
+      $("div.advanced").show();
+      $("#end-message").show();
+    } else if (language === "python" || language === "advanced-js") {
+      $("#free").show();
+      $("#pdx-code-guild").show();
       $("#end-message").show();
     }
 
-    // if (language === "python" || language === "advanced-js") {
-    //   $("#pdx-code-guild").show();
+     else if (cost === "code-camp" && learningEnvironment === "online") {
+      $("#free").show();
+      $("div.online-result").show();
+      $("#end-message").show();
+    } else if (cost === "code-camp") {
+      $("div.bootcamp-result").show();
+      $("#end-message").show();
+    } else {
+      $("#free").show();
+      $("div.bootcamp-result").show();
+      $("#end-message").show();
+    }
+
+
+
+
+    //
+    // if (experience === "advanced") {
+    //   $("#alchemy-code-lab").show();
     //   $("#end-message").show();
     // }
+    //
+    // // if (language === "python" || language === "advanced-js") {
+    // //   $("#pdx-code-guild").show();
+    // //   $("#end-message").show();
+    // // }
 
     event.preventDefault();
-
   });
 
 
-  $("button#view-free").click(function() {
-    $("#free").show();
-  });
+  // $("button#view-free").click(function() {
+  //   $("#free").show();
+  // });
 
   $("button#try-again").click(function() {
     location.reload(true);
@@ -58,28 +91,28 @@ $(document).ready(function() {
 });
 
 
-$(document).ready(function() {
-
-$("form#quiz").submit(function(event) {
-
-var answer = $("input:radio[name=gayorstraight]:checked").val();
-
-console.log(answer);
-
-if (answer === "gay") {
-  $("div#gay-message").show();
-} else {
-  $("div#straight-message").show();
-}
-
-/*$(".hidden").show();
-$("#gay-message").show();*/
-
-event.preventDefault();
-});
-
-$("button#try-again").click(function() {
-location.reload(true);
-});
-
-});
+// $(document).ready(function() {
+//
+// $("form#quiz").submit(function(event) {
+//
+// var answer = $("input:radio[name=gayorstraight]:checked").val();
+//
+// console.log(answer);
+//
+// if (answer === "gay") {
+//   $("div#gay-message").show();
+// } else {
+//   $("div#straight-message").show();
+// }
+//
+// /*$(".hidden").show();
+// $("#gay-message").show();*/
+//
+// event.preventDefault();
+// });
+//
+// $("button#try-again").click(function() {
+// location.reload(true);
+// });
+//
+// });
